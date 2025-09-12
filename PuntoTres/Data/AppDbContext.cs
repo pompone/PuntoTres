34193132c254803cc058efe27f98d784401935a8
pompone <54503871+pompone@users.noreply.github.com>
2025-09-12 18:23:52 -0300

    segundaSubida

---
+++ PuntoTres/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PuntoTres.Models;
+
+namespace PuntoTres.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<SolucionPreparada> SolucionesPreparadas { get; set; }
+    }
+}
